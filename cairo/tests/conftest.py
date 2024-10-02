1c2a6f51b5ce1f913c988aabc598e7996fe24f96
Clément Walter <clement0walter@gmail.com>
2024-10-02 17:05:08 +0300

    Fix pytest plugins deprecation

--- cairo/tests/conftest.py
+++ cairo/tests/conftest.py
@@ -1,52 +1,15 @@
 import logging
-import os
-
-os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-
 import shutil
 from pathlib import Path
 
 import pytest
-import starkware.cairo.lang.instances as LAYOUTS
 from dotenv import load_dotenv
-from hypothesis import Phase, Verbosity, settings
 
 from tests.utils.coverage import report_runs
 from tests.utils.reporting import dump_coverage
 
 load_dotenv()
 logger = logging.getLogger()
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--profile-cairo",
-        action="store_true",
-        default=False,
-        help="compute and dump TracerData for the VM runner: True or False",
-    )
-    parser.addoption(
-        "--proof-mode",
-        action="store_true",
-        default=False,
-        help="run the CairoRunner in proof mode: True or False",
-    )
-    parser.addoption(
-        "--layout",
-        choices=dir(LAYOUTS),
-        default="all_cairo_instance",
-        help="The layout of the Cairo AIR.",
-    )
-    parser.addoption(
-        "--seed",
-        action="store",
-        default=None,
-        type=int,
-        help="The seed to set random with.",
-    )
-
-
-pytest_plugins = ["tests.fixtures.runner", "tests.fixtures.data"]
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -72,31 +35,3 @@ async def coverage(worker_id):
     output_dir.mkdir(exist_ok=True, parents=True)
     shutil.rmtree(output_dir, ignore_errors=True)
     dump_coverage(output_dir, files)
-
-
-settings.register_profile(
-    "nightly",
-    deadline=None,
-    max_examples=1500,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "ci",
-    deadline=None,
-    max_examples=100,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "dev",
-    deadline=None,
-    max_examples=10,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "debug",
-    max_examples=10,
-    verbosity=Verbosity.verbose,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
-logger.info(f"Using Hypothesis profile: {os.getenv('HYPOTHESIS_PROFILE', 'default')}")
