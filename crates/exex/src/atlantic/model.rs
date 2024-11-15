f1f54f43649f7cde07d8ed90e736670a61635562
Thomas Coratger <thomas.coratger@gmail.com>
2024-11-15 10:50:16 +0100

    serde rename_all camel case

--- crates/exex/src/atlantic/model.rs
+++ crates/exex/src/atlantic/model.rs
@@ -1,8 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct Context {
-    #[serde(rename = "proofPath")]
     pub proof_path: Option<String>,
 }
 
