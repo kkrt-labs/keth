66b955cf4a48f9508a0283efaf486d3dccd9cae3
Clément Walter <clement0walter@gmail.com>
2025-01-03 10:05:26 +0100

    Runner.segments

--- python/cairo-addons/tests/test_memory_segments.py
+++ python/cairo-addons/tests/test_memory_segments.py
@@ -1,39 +1,38 @@
-from cairo_addons.vm import MemorySegmentManager, Relocatable
+import pytest
+from cairo_addons.vm import CairoRunner, Relocatable
+
+
+@pytest.fixture
+def runner(rust_program):
+    return CairoRunner(rust_program)
 
 
 class TestMemorySegmentManager:
-    def test_create_memory_segment_manager(self):
-        memory = MemorySegmentManager()
-        assert memory is not None
-
-    def test_add_segment(self):
-        memory = MemorySegmentManager()
-        ptr = memory.add()
+    def test_add_segment(self, runner):
+        ptr = runner.segments.add()
         assert isinstance(ptr, Relocatable)
         assert ptr.segment_index == 0
         assert ptr.offset == 0
 
-    def test_add_temporary_segment(self):
-        memory = MemorySegmentManager()
-        ptr = memory.add_temporary_segment()
+    def test_add_temporary_segment(self, runner):
+        ptr = runner.segments.add_temporary_segment()
         assert isinstance(ptr, Relocatable)
         assert ptr.segment_index == -1
         assert ptr.offset == 0
 
-    def test_load_data(self):
-        memory = MemorySegmentManager()
-        ptr = memory.add()
+    def test_load_data(self, runner):
+        ptr = runner.segments.add()
         data = [1, 2, 3, 4]
-        next_ptr = memory.load_data(ptr, data)
+        next_ptr = runner.segments.load_data(ptr, data)
         assert isinstance(next_ptr, Relocatable)
         assert next_ptr.segment_index == ptr.segment_index
         assert next_ptr.offset == 4
 
-    def test_compute_effective_sizes(self):
-        memory = MemorySegmentManager()
-        ptr = memory.add()
+    def test_compute_effective_sizes(self, runner):
+        ptr = runner.segments.add()
         data = [1, 2, 3, 4]
-        memory.load_data(ptr, data)
-        memory.compute_effective_sizes()
-        assert memory.get_segment_used_size(0) == 4
-        assert memory.get_segment_size(0) == 4
+        runner.segments.load_data(ptr, data)
+        sizes = runner.segments.compute_effective_sizes()
+        assert sizes == [4]
+        assert runner.segments.get_segment_used_size(0) == 4
+        assert runner.segments.get_segment_size(0) == 4
