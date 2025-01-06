cdbc24dcc4a05a2e05314a0b561b6a9b23de599c
Clément Walter <clement0walter@gmail.com>
2025-01-06 17:27:02 +0100

    Remove os.json compiled file

--- python/cairo-addons/tests/conftest.py
+++ python/cairo-addons/tests/conftest.py
@@ -4,6 +4,11 @@ from pathlib import Path
 import pytest
 from cairo_addons.vm import Program as RustProgram
 from hypothesis import strategies as st
+from starkware.cairo.lang.cairo_constants import DEFAULT_PRIME
+from starkware.cairo.lang.compiler.cairo_compile import compile_cairo, get_module_reader
+from starkware.cairo.lang.compiler.preprocessor.default_pass_manager import (
+    default_pass_manager,
+)
 from starkware.cairo.lang.compiler.program import Program as SWProgram
 from starkware.cairo.lang.vm.relocatable import RelocatableValue
 
@@ -22,18 +27,24 @@ st.register_type_strategy(
 )
 
 
-@pytest.fixture(scope="module")
-def program_path():
-    return Path(__file__).parent / "os.json"
+@pytest.fixture(scope="session")
+def sw_program():
+    module_reader = get_module_reader(cairo_path=[str(Path(__file__).parents[2])])
+
+    pass_manager = default_pass_manager(
+        prime=DEFAULT_PRIME, read_module=module_reader.read
+    )
+
+    return compile_cairo(
+        (Path(__file__).parent / "os.cairo").read_text(),
+        pass_manager=pass_manager,
+        debug_info=False,
+        add_start=False,
+    )
 
 
 @pytest.fixture(scope="module")
-def sw_program(program_path):
-    return SWProgram.load(data=json.loads(program_path.read_text()))
-
-
-@pytest.fixture(scope="module")
-def program_bytes(sw_program):
+def program_bytes(sw_program: SWProgram):
     return json.dumps(sw_program.Schema().dump(sw_program)).encode()
 
 
---
+++ python/cairo-addons/tests/os.cairo
@@ -0,0 +1,9 @@
+func main() {
+    os();
+
+    return ();
+}
+
+func os() -> (felt, felt, felt) {
+    return (1, 2, 3);
+}
