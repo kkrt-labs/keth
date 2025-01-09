fd0fcf938e2994548ce671e4d52e3a01f30827da
Elias Tazartes <elias.tazartes@gmail.com>
2025-01-09 13:10:46 +0100

    chore: delete comment in test

--- cairo/tests/ethereum/cancun/vm/instructions/test_stack.py
+++ cairo/tests/ethereum/cancun/vm/instructions/test_stack.py
@@ -12,9 +12,6 @@ from tests.utils.strategies import evm_lite
 class TestPushN:
     @given(evm=evm_lite, num_bytes=st.integers(min_value=0, max_value=32).map(Uint))
     def test_push_n(self, cairo_run, evm: Evm, num_bytes: Uint):
-        """
-        Test the push_n instruction by comparing Cairo and Python implementations
-        """
         try:
             cairo_result = cairo_run("push_n", evm, num_bytes)
         except ExceptionalHalt as cairo_error:
