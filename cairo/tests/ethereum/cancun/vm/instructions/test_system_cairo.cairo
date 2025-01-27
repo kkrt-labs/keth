8807c42af466b4b6a88e812426ef9a2dc5a50979
Elias Tazartes <elias.tazartes@gmail.com>
2025-01-27 11:46:30 +0100

    dev add tests

--- cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.cairo
+++ cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.cairo
@@ -1,9 +1,9 @@
 from starkware.cairo.common.cairo_builtins import BitwiseBuiltin, KeccakBuiltin, PoseidonBuiltin
 from starkware.cairo.common.registers import get_label_location
 
-from ethereum.cancun.vm.instructions.system import generic_create
+from ethereum.cancun.vm.instructions.system import generic_create, generic_call
 from ethereum.cancun.vm.interpreter import process_create_message, process_message
-from ethereum_types.numeric import U256, Uint
+from ethereum_types.numeric import U256, Uint, bool
 from ethereum.cancun.fork_types import Address
 from ethereum.cancun.vm.exceptions import EthereumException
 from ethereum.cancun.vm import Evm
@@ -25,6 +25,42 @@ func test_generic_create{
     let (process_message_label) = get_label_location(process_message);
     let res = generic_create{process_create_message_label=process_create_message_label, evm=evm}(
         endowment, contract_address, memory_start_position, memory_size, init_code_gas
+    );
+    return res;
+}
+
+func test_generic_call{
+    range_check_ptr,
+    bitwise_ptr: BitwiseBuiltin*,
+    keccak_ptr: KeccakBuiltin*,
+    poseidon_ptr: PoseidonBuiltin*,
+    evm: Evm,
+}(
+    gas: Uint,
+    value: U256,
+    caller: Address,
+    to: Address,
+    code_address: Address,
+    should_transfer_value: bool,
+    is_staticcall: bool,
+    memory_input_start_position: U256,
+    memory_input_size: U256,
+    memory_output_start: U256,
+    memory_output_size: U256,
+) -> EthereumException* {
+    let (process_message_label) = get_label_location(process_message);
+    let res = generic_call{process_message_label=process_message_label, evm=evm}(
+        gas,
+        value,
+        caller,
+        to,
+        code_address,
+        should_transfer_value,
+        is_staticcall,
+        memory_input_start_position,
+        memory_input_size,
+        memory_output_start,
+        memory_output_size,
     );
     return res;
 }
--- cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.py
+++ cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.py
@@ -2,7 +2,7 @@ from ethereum_types.numeric import U256, Uint
 from hypothesis import given
 
 from ethereum.cancun.fork_types import Address
-from ethereum.cancun.vm.instructions.system import generic_create
+from ethereum.cancun.vm.instructions.system import generic_call, generic_create
 from tests.utils.args_gen import Evm
 from tests.utils.errors import strict_raises
 from tests.utils.evm_builder import EvmBuilder
@@ -70,5 +70,85 @@ class TestSystem:
             memory_start_position,
             memory_size,
             init_code_gas,
+        )
+        assert evm == cairo_evm
+
+    @given(
+        evm=local_strategy,
+        gas=...,
+        value=...,
+        caller=...,
+        to=...,
+        code_address=...,
+        should_transfer_value=...,
+        is_staticcall=...,
+        memory_input_start_position=bounded_u256_strategy(max_value=MAX_MEMORY_SIZE),
+        memory_input_size=bounded_u256_strategy(max_value=MAX_MEMORY_SIZE),
+        memory_output_start_position=bounded_u256_strategy(max_value=MAX_MEMORY_SIZE),
+        memory_output_size=bounded_u256_strategy(max_value=MAX_MEMORY_SIZE),
+    )
+    def test_generic_call(
+        self,
+        cairo_run,
+        evm: Evm,
+        gas: Uint,
+        value: U256,
+        caller: Address,
+        to: Address,
+        code_address: Address,
+        should_transfer_value: bool,
+        is_staticcall: bool,
+        memory_input_start_position: U256,
+        memory_input_size: U256,
+        memory_output_start_position: U256,
+        memory_output_size: U256,
+    ):
+        try:
+            cairo_evm = cairo_run(
+                "test_generic_call",
+                evm,
+                gas,
+                value,
+                caller,
+                to,
+                code_address,
+                should_transfer_value,
+                is_staticcall,
+                memory_input_start_position,
+                memory_input_size,
+                memory_output_start_position,
+                memory_output_size,
+            )
+        except Exception as cairo_error:
+            with strict_raises(type(cairo_error)):
+                generic_call(
+                    evm,
+                    gas,
+                    value,
+                    caller,
+                    to,
+                    code_address,
+                    should_transfer_value,
+                    is_staticcall,
+                    memory_input_start_position,
+                    memory_input_size,
+                    memory_output_start_position,
+                    memory_output_size,
+                )
+            return
+
+        generic_call(
+            evm,
+            gas,
+            value,
+            caller,
+            to,
+            code_address,
+            should_transfer_value,
+            is_staticcall,
+            memory_input_start_position,
+            memory_input_size,
+            memory_output_start_position,
+            memory_output_size,
         )
         assert evm == cairo_evm
