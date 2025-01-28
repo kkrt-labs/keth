249744b3b1619943a18428c8b2c3fab69017b427
Elias Tazartes <elias.tazartes@gmail.com>
2025-01-28 10:26:00 +0100

    try a ci run

--- cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.py
+++ cairo/tests/ethereum/cancun/vm/instructions/test_system_cairo.py
@@ -1,5 +1,5 @@
 from ethereum_types.numeric import U256, Uint
-from hypothesis import given, reproduce_failure
+from hypothesis import given
 
 from ethereum.cancun.fork_types import Address
 from ethereum.cancun.vm.instructions.system import (
