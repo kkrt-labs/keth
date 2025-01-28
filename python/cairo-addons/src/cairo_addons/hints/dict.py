30d5686b215e5a994d988f65240b8eaad87e28db
Oba <obatirou@gmail.com>
2025-01-28 18:23:06 +0100

    trunk fmt

--- python/cairo-addons/src/cairo_addons/hints/dict.py
+++ python/cairo-addons/src/cairo_addons/hints/dict.py
@@ -1,9 +1,10 @@
-from cairo_addons.hints.decorator import register_hint
 from starkware.cairo.common.dict import DictManager
 from starkware.cairo.lang.vm.memory_dict import MemoryDict
 from starkware.cairo.lang.vm.memory_segments import MemorySegmentManager
 from starkware.cairo.lang.vm.relocatable import RelocatableValue
 from starkware.cairo.lang.vm.vm_consts import VmConsts
+
+from cairo_addons.hints.decorator import register_hint
 
 
 @register_hint
