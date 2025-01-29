e7466071e206dd5eb4e7c1cfcf88ec80bfa388e5
Elias Tazartes <elias.tazartes@gmail.com>
2025-01-29 12:11:57 +0100

    fix remove try logic

--- cairo/tests/ethereum/cancun/test_transactions.py
+++ cairo/tests/ethereum/cancun/test_transactions.py
@@ -36,59 +36,25 @@ class TestTransactions:
 
     @given(tx=...)
     def test_signing_hash_pre155(self, cairo_run, tx: LegacyTransaction):
-        """Test pre-EIP155 transaction signing hash computation"""
-        try:
-            cairo_result = cairo_run("signing_hash_pre155", tx)
-        except Exception as cairo_error:
-            with strict_raises(type(cairo_error)):
-                signing_hash_pre155(tx)
-            return
-
+        cairo_result = cairo_run("signing_hash_pre155", tx)
         assert signing_hash_pre155(tx) == cairo_result
 
     @given(tx=..., chain_id=...)
     def test_signing_hash_155(self, cairo_run, tx: LegacyTransaction, chain_id: U64):
-        """Test EIP-155 transaction signing hash computation"""
-        try:
-            cairo_result = cairo_run("signing_hash_155", tx, chain_id)
-        except Exception as cairo_error:
-            with strict_raises(type(cairo_error)):
-                signing_hash_155(tx, chain_id)
-            return
-
+        cairo_result = cairo_run("signing_hash_155", tx, chain_id)
         assert signing_hash_155(tx, chain_id) == cairo_result
 
     @given(tx=...)
     def test_signing_hash_2930(self, cairo_run, tx: AccessListTransaction):
-        """Test EIP-2930 transaction signing hash computation"""
-        try:
-            cairo_result = cairo_run("signing_hash_2930", tx)
-        except Exception as cairo_error:
-            with strict_raises(type(cairo_error)):
-                signing_hash_2930(tx)
-            return
-
+        cairo_result = cairo_run("signing_hash_2930", tx)
         assert signing_hash_2930(tx) == cairo_result
 
     @given(tx=...)
     def test_signing_hash_1559(self, cairo_run, tx: FeeMarketTransaction):
-        """Test EIP-1559 transaction signing hash computation"""
-        try:
-            cairo_result = cairo_run("signing_hash_1559", tx)
-        except Exception as cairo_error:
-            with strict_raises(type(cairo_error)):
-                signing_hash_1559(tx)
-            return
-
+        cairo_result = cairo_run("signing_hash_1559", tx)
         assert signing_hash_1559(tx) == cairo_result
 
     @given(tx=...)
     def test_signing_hash_4844(self, cairo_run, tx: BlobTransaction):
-        """Test EIP-4844 transaction signing hash computation"""
-        try:
-            cairo_result = cairo_run("signing_hash_4844", tx)
-        except Exception as cairo_error:
-            with strict_raises(type(cairo_error)):
-                signing_hash_4844(tx)
-            return
+        cairo_result = cairo_run("signing_hash_4844", tx)
         assert signing_hash_4844(tx) == cairo_result
