2a8ad5edcb883f5881f0c4517dd54a63e82da872
Elias Tazartes <elias.tazartes@gmail.com>
2025-02-05 15:02:19 +0100

    fix comments

--- cairo/tests/ethereum/cancun/vm/test_runtime.py
+++ cairo/tests/ethereum/cancun/vm/test_runtime.py
@@ -1,10 +1,14 @@
 from ethereum.cancun.vm.runtime import get_valid_jump_destinations
 from ethereum_types.bytes import Bytes
-from hypothesis import given
+from hypothesis import example, given
+
+from tests.utils.solidity import get_contract
 
 
 class TestRuntime:
-    @given(code=contract_code_strategy())
+    @given(code=...)
+    @example(code=get_contract("Counter", "Counter").bytecode_runtime)
+    @example(code=get_contract("ERC20", "KethToken").bytecode_runtime)
     def test_get_valid_jump_destinations(self, cairo_run, code: Bytes):
 
         cairo_result = cairo_run("get_valid_jump_destinations", code)
