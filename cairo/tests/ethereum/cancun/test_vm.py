6ddeae65a4953a55d97042efcd0af76359646933
Elias Tazartes <elias.tazartes@gmail.com>
2025-02-06 17:41:55 +0100

    cov: add coverage for test_incorporate_child_on_success_with_empty_account

--- cairo/tests/ethereum/cancun/test_vm.py
+++ cairo/tests/ethereum/cancun/test_vm.py
@@ -1,3 +1,5 @@
+from ethereum.cancun.fork_types import EMPTY_ACCOUNT
+from ethereum.cancun.state import set_account
 from ethereum.cancun.vm import (
     Evm,
     incorporate_child_on_error,
@@ -44,4 +46,40 @@ class TestVm:
             return
 
         incorporate_child_on_error(evm, child_evm)
+        assert evm_cairo == evm
+
+    @given(
+        evm=EvmBuilder()
+        .with_gas_left()
+        .with_logs()
+        .with_accessed_addresses()
+        .with_accessed_storage_keys()
+        .with_accounts_to_delete()
+        .with_touched_accounts()
+        .with_refund_counter()
+        .with_env()
+        .build(),
+        child_evm=EvmBuilder()
+        .with_gas_left()
+        .with_logs()
+        .with_accessed_addresses()
+        .with_accessed_storage_keys()
+        .with_accounts_to_delete()
+        .with_touched_accounts()
+        .with_refund_counter()
+        .with_env()
+        .build(),
+    )
+    def test_incorporate_child_on_success_with_empty_account(
+        self, cairo_run, evm: Evm, child_evm: Evm
+    ):
+        set_account(evm.env.state, child_evm.message.current_target, EMPTY_ACCOUNT)
+        try:
+            evm_cairo = cairo_run("incorporate_child_on_success", evm, child_evm)
+        except Exception as e:
+            with strict_raises(type(e)):
+                incorporate_child_on_success(evm, child_evm)
+            return
+
+        incorporate_child_on_success(evm, child_evm)
         assert evm_cairo == evm
