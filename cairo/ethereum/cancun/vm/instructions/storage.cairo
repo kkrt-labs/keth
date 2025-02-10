3a043e0eccb34d10fb823f3ce1b799fb687a84ce
Oba <obatirou@gmail.com>
2025-02-10 14:30:23 +0100

    cov: refund counter sstore

--- cairo/ethereum/cancun/vm/instructions/storage.cairo
+++ cairo/ethereum/cancun/vm/instructions/storage.cairo
@@ -20,7 +20,7 @@ from ethereum.cancun.fork_types import (
     Address,
 )
 from ethereum_types.bytes import Bytes32
-from ethereum_types.numeric import Uint, U256, U256Struct
+from ethereum_types.numeric import Uint, U256, U256Struct, bool
 from ethereum.utils.numeric import U256_to_be_bytes
 from legacy.utils.dict import hashdict_read, hashdict_write
 from legacy.utils.utils import Helpers
@@ -208,27 +208,22 @@ func sstore{
     }
     let gas_cost = [ap - 1];
 
-    tempvar refund_counter = evm.value.refund_counter;
-    let is_original_eq_new = U256__eq__(original_value, new_value);
+    local current_refund_counter = evm.value.refund_counter;
+    local refund_counter;
     // Refund calculation
     if (is_current_eq_new.value == 0) {
-        let is_current_zero = U256__eq__(current_value, zero_u256);
-        let is_new_zero = U256__eq__(new_value, zero_u256);
-        if (is_original_zero.value == 0 and is_current_zero.value == 0 and is_new_zero.value != 0) {
-            refund_counter = refund_counter + GasConstants.GAS_STORAGE_CLEAR_REFUND;
-        }
-        if (is_original_zero.value == 0 and is_current_zero.value != 0) {
-            refund_counter = refund_counter - GasConstants.GAS_STORAGE_CLEAR_REFUND;
-        }
-        if (is_original_eq_new.value != 0) {
-            if (is_original_zero.value != 0) {
-                refund_counter = refund_counter +
-                    (GasConstants.GAS_STORAGE_SET - GasConstants.GAS_WARM_ACCESS);
-            } else {
-                refund_counter = refund_counter +
-                    (GasConstants.GAS_STORAGE_UPDATE - GasConstants.GAS_COLD_SLOAD - GasConstants.GAS_WARM_ACCESS);
-            }
-        }
+        let refund_counter_res = _calculate_refund_counter_current_eq_new(
+            is_original_zero,
+            is_original_eq_current,
+            current_refund_counter,
+            original_value,
+            current_value,
+            new_value,
+            zero_u256,
+        );
+        assert refund_counter = refund_counter_res;
+    } else {
+        assert refund_counter = current_refund_counter;
     }
 
     // Charge gas
@@ -270,6 +265,55 @@ func sstore{
     EvmImpl.set_accessed_storage_keys(new_accessed_storage_keys);
     let ok = cast(0, EthereumException*);
     return ok;
+}
+
+// @notice Calculates the refund counter for sstore.
+func _calculate_refund_counter_current_eq_new(
+    is_original_zero: bool,
+    is_original_eq_current: bool,
+    current_refund_counter: felt,
+    original_value: U256,
+    current_value: U256,
+    new_value: U256,
+    zero_u256: U256,
+) -> felt {
+    alloc_locals;
+
+    local refund_counter;
+    let is_original_eq_new = U256__eq__(original_value, new_value);
+    let is_current_zero = U256__eq__(current_value, zero_u256);
+    let is_new_zero = U256__eq__(new_value, zero_u256);
+
+    if (is_original_zero.value == 0 and is_current_zero.value == 0 and is_new_zero.value != 0) {
+        assert refund_counter = current_refund_counter + GasConstants.GAS_STORAGE_CLEAR_REFUND;
+        return refund_counter;
+    }
+
+    local temp_refund_counter;
+    if (is_original_zero.value == 0) {
+        if (is_current_zero.value != 0) {
+            assert temp_refund_counter = current_refund_counter -
+                GasConstants.GAS_STORAGE_CLEAR_REFUND;
+        }
+    } else {
+        assert temp_refund_counter = 0;
+    }
+    if (is_original_eq_new.value != 0) {
+        if (is_original_zero.value != 0) {
+            assert refund_counter = temp_refund_counter + current_refund_counter + (
+                GasConstants.GAS_STORAGE_SET - GasConstants.GAS_WARM_ACCESS
+            );
+        } else {
+            assert refund_counter = temp_refund_counter + current_refund_counter + (
+                GasConstants.GAS_STORAGE_UPDATE -
+                GasConstants.GAS_COLD_SLOAD -
+                GasConstants.GAS_WARM_ACCESS
+            );
+        }
+    } else {
+        assert refund_counter = current_refund_counter + temp_refund_counter;
+    }
+    return refund_counter;
 }
 
 // @notice Loads to the stack the value corresponding to a certain key from the
--- cairo/tests/ethereum/cancun/vm/instructions/test_storage.py
+++ cairo/tests/ethereum/cancun/vm/instructions/test_storage.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from ethereum.cancun.fork_types import Account, Address
 from ethereum.cancun.state import set_account, set_storage
 from ethereum.cancun.trie import copy_trie
@@ -5,13 +7,15 @@ from ethereum.cancun.vm import Evm
 from ethereum.cancun.vm.instructions.storage import sload, sstore, tload, tstore
 from ethereum.cancun.vm.stack import push
 from ethereum_types.bytes import Bytes32
-from ethereum_types.numeric import U256
+from ethereum_types.numeric import U256, Uint
 from hypothesis import given
 from hypothesis import strategies as st
+from hypothesis.strategies import composite
 
 from tests.utils.errors import strict_raises
 from tests.utils.evm_builder import EvmBuilder
 from tests.utils.message_builder import MessageBuilder
+from tests.utils.strategies import account_strategy
 
 evm_storage_strategy = (
     EvmBuilder()
@@ -22,6 +26,30 @@ evm_storage_strategy = (
     .with_message(MessageBuilder().with_is_static().build())
     .build()
 )
+
+
+@composite
+def sstore_strategy(draw):
+    evm = draw(evm_storage_strategy)
+    is_key_accessed = draw(st.booleans())
+    if is_key_accessed and evm.stack:
+        account = draw(account_strategy)
+        # Ensure the account exists and the key is accessed
+        set_account(
+            evm.env.state,
+            evm.message.current_target,
+            account,
+        )
+        evm.accessed_storage_keys.add(
+            (evm.message.current_target, evm.stack[-1].to_be_bytes32())
+        )
+    return evm
+
+
+@composite
+def sstore_refund_counter_strategy(draw):
+    tuples = list(product([U256(0), U256(1)], repeat=3))
+    return draw(st.sampled_from(tuples))
 
 
 class TestStorage:
@@ -37,7 +65,7 @@ class TestStorage:
         sload(evm)
         assert evm == cairo_evm
 
-    @given(evm=evm_storage_strategy)
+    @given(evm=sstore_strategy())
     def test_sstore(self, cairo_run, evm: Evm):
         try:
             cairo_evm = cairo_run("sstore", evm)
@@ -49,37 +77,42 @@ class TestStorage:
         assert evm == cairo_evm
 
     @given(
-        evm=evm_storage_strategy,
-        key=...,
-        value=...,
+        evm=EvmBuilder().build(),
         address=...,
-        account=...,
-        current_value_is_zero=...,
+        key=...,
+        data=sstore_refund_counter_strategy(),
     )
-    def test_sstore_with_target_address_in_accessed_storage_keys_and_current_value_diff_new_value(
+    def test_sstore_refund_counter(
         self,
         cairo_run,
-        evm: Evm,
-        key: Bytes32,
-        value: U256,
         address: Address,
-        account: Account,
-        current_value_is_zero: bool,
+        key: Bytes32,
+        evm: Evm,
+        data,
     ):
+        """
+        This test ensures all cases for the refund counter are tested.
+        That is why the evm is not fuzzed.
+        """
+        evm.gas_left = Uint(30_000_000)  # avoid OutOfGasError
+        new_value, current_value, original_value = data
+
+        # Ensure the account exists and is the current target
+        account = Account(balance=U256(0), nonce=U256(0), code=b"6001600101")
         set_account(
             evm.env.state,
             address,
             account,
         )
         evm.message.current_target = address
-        evm.accessed_storage_keys.add((evm.message.current_target, key))
+        # Set the original value
         set_storage(
             evm.env.state,
             evm.message.current_target,
             key,
-            value if not current_value_is_zero else U256(0),
+            original_value,
         )
-        # modify snapshot to match the new state
+        # Take a snapshot of the state
         evm.env.state._snapshots.insert(
             0,
             (
@@ -90,7 +123,15 @@ class TestStorage:
                 },
             ),
         )
-        push(evm.stack, value)
+        # Set the current value
+        set_storage(
+            evm.env.state,
+            evm.message.current_target,
+            key,
+            current_value,
+        )
+        # Push the new value and the key to the stack
+        push(evm.stack, new_value)
         push(evm.stack, U256.from_be_bytes(key))
 
         try:
