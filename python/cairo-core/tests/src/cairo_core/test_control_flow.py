4dc8abe88c1cabbd4312fb5bedd9f8398ed0e1ae
Clément Walter <clement0walter@gmail.com>
2025-02-10 14:53:10 +0100

    Test raise util only woth ascii letters

--- python/cairo-core/tests/src/cairo_core/test_control_flow.py
+++ python/cairo-core/tests/src/cairo_core/test_control_flow.py
@@ -1,3 +1,5 @@
+from string import ascii_letters
+
 from hypothesis import given
 from hypothesis import strategies as st
 
@@ -5,7 +7,7 @@ from cairo_addons.testing.errors import cairo_error
 
 
 class TestControlFlow:
-    @given(message=st.text(st.characters(codec="ascii"), min_size=0, max_size=31))
+    @given(message=st.text(alphabet=ascii_letters, min_size=0, max_size=31))
     def test_raise(self, cairo_run, message):
         with cairo_error(message=message):
             cairo_run("raise", int.from_bytes(message.encode(), "big"))
