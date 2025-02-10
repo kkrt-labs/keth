412daf272e01f115f7699d1c3c31a4fd651acd80
Oba <obatirou@gmail.com>
2025-02-10 18:42:09 +0100

    cov: fork_types (#750)
    Close #733

--- cairo/tests/ethereum/cancun/test_fork_types.py
+++ cairo/tests/ethereum/cancun/test_fork_types.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from ethereum.cancun.fork_types import EMPTY_ACCOUNT, Account
-from hypothesis import given
+from ethereum_types.numeric import U256, Uint
+from hypothesis import example, given
 
 
 class TestForkTypes:
@@ -7,7 +10,17 @@ class TestForkTypes:
         assert EMPTY_ACCOUNT == cairo_run("EMPTY_ACCOUNT")
 
     @given(account_a=..., account_b=...)
-    def test_account_eq(self, cairo_run, account_a: Account, account_b: Account):
+    @example(
+        account_a=Account(nonce=Uint(1), balance=U256(2**128), code=bytearray()),
+        account_b=Account(nonce=Uint(1), balance=U256(2**129), code=bytearray()),
+    )
+    @example(
+        account_a=EMPTY_ACCOUNT,
+        account_b=EMPTY_ACCOUNT,
+    )
+    def test_account_eq(
+        self, cairo_run, account_a: Optional[Account], account_b: Optional[Account]
+    ):
         assert (account_a == account_b) == cairo_run(
             "Account__eq__", account_a, account_b
         )
