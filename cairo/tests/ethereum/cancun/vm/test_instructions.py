fe36376046b1670523935b49d777059540dfe6f4
Oba <obatirou@gmail.com>
2025-02-11 10:46:29 +0100

    cov: instructions bis (#753)
    Close #741

--- cairo/tests/ethereum/cancun/vm/test_instructions.py
+++ cairo/tests/ethereum/cancun/vm/test_instructions.py
@@ -1,14 +1,28 @@
 from ethereum.cancun.vm import Evm
 from ethereum.cancun.vm.exceptions import InvalidOpcode
 from ethereum.cancun.vm.instructions import Ops, op_implementation
+from ethereum_types.numeric import U256, Uint
 from hypothesis import given
+from hypothesis import strategies as st
 
 from cairo_addons.testing.errors import strict_raises
+from tests.utils.args_gen import Stack
 from tests.utils.evm_builder import EvmBuilder
 from tests.utils.strategies import uint8
 
 
-@given(evm=EvmBuilder().with_stack().with_gas_left().build(), opcode=uint8)
+# Limit the fuzzing to the minimum necessary to ensure all opcodes are tested
+@given(
+    evm=EvmBuilder()
+    .with_stack(
+        st.lists(st.from_type(U256), min_size=0, max_size=10).map(
+            lambda x: Stack[U256](x)
+        )
+    )
+    .with_gas_left(st.just(Uint(1000000)))
+    .build(),
+    opcode=uint8,
+)
 def test_op_implementation(cairo_run, evm: Evm, opcode):
     try:
         cairo_evm = cairo_run("test_op_implementation", evm, opcode)
