1e051548c9b824cac91a3ae5e038f2fc6273134a
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-03-05 15:06:06 +0100

    test: add unit test for sign_to_uint384_mod_alt_bn128

--- python/cairo-ec/tests/curve/test_alt_bn128.py
+++ python/cairo-ec/tests/curve/test_alt_bn128.py
@@ -2,6 +2,8 @@ import pytest
 from ethereum.crypto.alt_bn128 import ALT_BN128_CURVE_ORDER as AltBn128N
 from ethereum.crypto.alt_bn128 import BNF as AltBn128P
 from ethereum.crypto.alt_bn128 import BNP as AltBn128
+from hypothesis import given
+from hypothesis import strategies as st
 
 from cairo_addons.utils.uint256 import uint256_to_int
 from cairo_addons.utils.uint384 import uint384_to_int
@@ -41,3 +43,19 @@ class TestAltBn128:
                 uint384_to_int(*cairo_run("test__get_P_MIN_ONE").values())
                 == AltBn128P.PRIME - 1
             )
+
+        @given(scenario=st.sampled_from(["negative", "positive"]))
+        def test_sign_to_uint384_mod_alt_bn128(self, cairo_run, scenario):
+            if scenario == "negative":
+                sign = -1
+                expected = AltBn128P.PRIME - 1
+            if scenario == "positive":
+                sign = 1
+                expected = 1
+
+            res = cairo_run(
+                "test__sign_to_uint384_mod_alt_bn128",
+                sign=sign,
+            )
+
+            assert uint384_to_int(*res.values()) == expected
