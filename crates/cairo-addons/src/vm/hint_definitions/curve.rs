f8ede247026646da3933cb683a8e012cdd9af3d4
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-03-21 16:14:02 +0100

    chore: remove unused import

--- crates/cairo-addons/src/vm/hint_definitions/curve.rs
+++ crates/cairo-addons/src/vm/hint_definitions/curve.rs
@@ -3,8 +3,7 @@ use std::{cmp::min, collections::HashMap};
 use cairo_vm::{
     hint_processor::{
         builtin_hint_processor::hint_utils::{
-            get_integer_from_var_name, get_ptr_from_var_name, get_relocatable_from_var_name,
-            insert_value_from_var_name,
+            get_integer_from_var_name, get_ptr_from_var_name, insert_value_from_var_name,
         },
         hint_processor_definition::HintReference,
     },
