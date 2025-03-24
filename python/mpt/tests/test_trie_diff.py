d9432a56d917a6244084447eac73fdb84fd3b61d
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2025-03-24 09:55:45 +0100

    Update python/mpt/tests/test_trie_diff.py
    Co-authored-by: Mathieu <60658558+enitrat@users.noreply.github.com>

--- python/mpt/tests/test_trie_diff.py
+++ python/mpt/tests/test_trie_diff.py
@@ -14,7 +14,7 @@ def ethereum_trie_transition_db(path):
 @pytest.mark.parametrize(
     "path",
     [
-        (Path("test-data/22081873.json")),
+        (Path("test_data/22081873.json")),
     ],
 )
 class TestTrieDiff:
