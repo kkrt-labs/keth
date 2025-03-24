89c84ed5b1a63d6bf8faedee692ecf3c6d45de11
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-03-24 15:35:00 +0100

    wip: bnf2 multiplicative inverse hint rust

--- crates/cairo-addons/src/vm/hint_definitions/crypto.rs
+++ crates/cairo-addons/src/vm/hint_definitions/crypto.rs
@@ -13,10 +13,15 @@ use cairo_vm::{
     Felt252,
 };
 
-use crate::vm::{hint_utils::Uint384, hints::Hint};
+use crate::vm::{
+    hint_utils::{write_collection_to_addr, Uint384},
+    hints::Hint,
+};
 
 pub const HINTS: &[fn() -> Hint] = &[bnf2_multiplicative_inverse];
 
+// Function adapted from the `substrate_bn` crate as not publicly available.
+// Link: https://github.com/paritytech/bn/blob/63f8c587356a67b33c7396af98e065b66fca5dda/src/fields/fq2.rs#L7
 fn fq_non_residue() -> Fq {
     // (q - 1) is a quadratic non-residue in Fq
     // 21888242871839275222246405745257275088696311157297823662689037894645226208582
@@ -29,8 +34,12 @@ fn fq_non_residue() -> Fq {
     .unwrap()
 }
 
+// Function adapted from the `substrate_bn` crate as not publicly available.
+// Link: https://github.com/paritytech/bn/blob/63f8c587356a67b33c7396af98e065b66fca5dda/src/fields/fq2.rs#L119
+// TODO: Investigate why not available,
 fn fq2_inverse(a: &Fq2) -> Option<Fq2> {
     // "High-Speed Software Implementation of the Optimal Ate Pairing
+    // over Barreto–Naehrig Curves"; Algorithm 8
     let c0 = a.real();
     let c1 = a.imaginary();
 
@@ -46,33 +55,45 @@ pub fn bnf2_multiplicative_inverse() -> Hint {
          ap_tracking: &ApTracking,
          _constants: &HashMap<String, Felt252>|
          -> Result<(), HintError> {
-            let a_addr = get_ptr_from_var_name("a", vm, ids_data, ap_tracking)?;
-            let c0_addr = vm.get_relocatable(a_addr)?;
-            let c1_addr = vm.get_relocatable((a_addr + 1_usize).unwrap())?;
-            let c0 = Uint384::from_base_addr(c0_addr, "a.c0", vm)?.pack();
-            let c1 = Uint384::from_base_addr(c1_addr, "a.c1", vm)?.pack();
+            let b_addr = get_ptr_from_var_name("b", vm, ids_data, ap_tracking)?;
+            let c0_addr = vm.get_relocatable(b_addr)?;
+            let c1_addr = vm.get_relocatable((b_addr + 1_usize).unwrap())?;
+            let c0 = Uint384::from_base_addr(c0_addr, "b.c0", vm)?.pack();
+            let c1 = Uint384::from_base_addr(c1_addr, "b.c1", vm)?.pack();
 
-            // To compute the multiplicative inverse, we use the substrate-bn package (also used by
-            // revm). A Fq2 struct is made of two Fq elements which can be constructed
-            // from a U256.
+            // To compute the multiplicative inverse, we use the `substrate_bn` crate (also used
+            // by revm). A Fq2 struct is made of two Fq elements which can be
+            // constructed from a U256.
+            // In this crate, bytes are expected to be in Big Endian (BE).
 
-            let c0_fq = Fq::from_slice(&c0.to_bytes_le()).unwrap();
-            let c1_fq = Fq::from_slice(&c1.to_bytes_le()).unwrap();
+            let c0_fq = Fq::from_slice(&c0.to_bytes_be()).unwrap();
+            let c1_fq = Fq::from_slice(&c1.to_bytes_be()).unwrap();
 
-            let a = Fq2::new(c0_fq, c1_fq);
-            let a_inv = fq2_inverse(&a).unwrap();
+            let b = Fq2::new(c0_fq, c1_fq);
+            let b_inv = fq2_inverse(&b).unwrap();
 
-            let a_inv_c0 = a_inv.real();
-            let a_inv_c1 = a_inv.imaginary();
-            let a_inv_c0_be_slice: &mut [u8] = &mut [];
-            let a_inv_c1_be_slice: &mut [u8] = &mut [];
-            a_inv_c0.to_big_endian(a_inv_c0_be_slice).unwrap();
-            a_inv_c1.to_big_endian(a_inv_c1_be_slice).unwrap();
+            let b_inv_c0 = b_inv.real();
+            let b_inv_c1 = b_inv.imaginary();
+            let b_inv_c0_be_slice: &mut [u8] = &mut [];
+            let b_inv_c1_be_slice: &mut [u8] = &mut [];
+            b_inv_c0.to_big_endian(b_inv_c0_be_slice).unwrap();
+            b_inv_c1.to_big_endian(b_inv_c1_be_slice).unwrap();
 
-            let c0_u384 = Uint384::split(&BigUint::from_bytes_be(&a_inv_c0_be_slice));
-            let c1_u384 = Uint384::split(&BigUint::from_bytes_be(&a_inv_c1_be_slice));
+            let c0_u384 = Uint384::split(&BigUint::from_bytes_be(b_inv_c0_be_slice));
+            let c1_u384 = Uint384::split(&BigUint::from_bytes_be(b_inv_c1_be_slice));
+            let bnf2_struct_ptr = vm.add_memory_segment();
+            let c0_ptr = vm.add_memory_segment();
+            let c1_ptr = vm.add_memory_segment();
 
-            insert_value_from_var_name("a_inv", Felt252::from(0), vm, ids_data, ap_tracking)?;
+            for i in 0..4 {
+                vm.insert_value((c0_ptr + i)?, c0_u384.limbs[i].clone().into_owned())?;
+            }
+            for i in 0..4 {
+                vm.insert_value((c1_ptr + i)?, c1_u384.limbs[i].clone().into_owned())?;
+            }
+            vm.insert_value(bnf2_struct_ptr, c0_ptr)?;
+            vm.insert_value((bnf2_struct_ptr + 1_usize).unwrap(), c1_ptr)?;
+            insert_value_from_var_name("b_inv", bnf2_struct_ptr, vm, ids_data, ap_tracking)?;
             Ok(())
         },
     )
