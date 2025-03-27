e35fc017cf3829ebbf917d68711ddca2bd86161c
Nabil Elqatib <nabil@kakarot.org>
2025-03-27 17:40:16 +0100

    fix trunk
    Signed-off-by: Nabil Elqatib <nabil@kakarot.org>

--- crates/cairo-addons/src/vm/hint_definitions/maths.rs
+++ crates/cairo-addons/src/vm/hint_definitions/maths.rs
@@ -19,12 +19,12 @@ use num_bigint::BigUint;
 use crate::vm::hints::Hint;
 
 pub const HINTS: &[fn() -> Hint] = &[
+    felt252_to_bits_rev,
     felt252_to_bytes_le,
     felt252_to_bytes_be,
     value_len_mod_two,
     is_positive_hint,
     value_len_mod_two,
-    felt252_to_bits_rev,
 ];
 
 pub fn felt252_to_bytes_le() -> Hint {
