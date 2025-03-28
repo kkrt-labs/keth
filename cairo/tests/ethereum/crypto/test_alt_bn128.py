a515dfb895605c5da6d4d162f6618b505bdafa52
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-03-28 17:08:32 +0100

    fix: rename bnf12 final exp test

--- cairo/tests/ethereum/crypto/test_alt_bn128.py
+++ cairo/tests/ethereum/crypto/test_alt_bn128.py
@@ -20,7 +20,7 @@ from cairo_ec.curve import AltBn128
 from tests.utils.args_gen import U384
 
 # https://github.com/keep-starknet-strange/garaga/blob/704a8c66bf85b965851a117c6b116fc7a11329db/hydra/garaga/definitions.py#L346
-# see test_bnp12_final_exponentiation
+# see test_bnf12_final_exponentiation
 GARAGA_COFACTOR = 0x3BEC47DF15E307C81EA96B02D9D9E38D2E5D4E223DDEDAF4
 
 
@@ -264,9 +264,9 @@ segments.load_data(ids.b_inv.address_, [bnf2_struct_ptr])
         @given(a=...)
         @settings(max_examples=10)
         @pytest.mark.slow
-        def test_bnp12_final_exponentiation(self, cairo_run_py, a: BNF12):
+        def test_bnf12_final_exponentiation(self, cairo_run_py, a: BNF12):
             assume(a != BNF12.zero())
-            assert cairo_run_py("bnp12_final_exponentiation", a) == a ** (
+            assert cairo_run_py("bnf12_final_exponentiation", a) == a ** (
                 ((ALT_BN128_PRIME**12 - 1) // ALT_BN128_CURVE_ORDER) * GARAGA_COFACTOR
             )
 
