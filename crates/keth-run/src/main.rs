9055105683a238f843558ad6cd9e4e0fc093e49f
Nabil Elqatib <nabil@kakarot.org>
2025-04-04 14:47:19 +0200

    working example with hardcoded values and calling py's run_proof_mode
    run with
    PYTHONPATH=/Users/nabil/dev/keth/.venv/lib/python3.10/site-packages:\
    /Users/nabil/dev/keth/python/eth-rpc/src:\
    /Users/nabil/dev/keth/python/mpt/src:\
    /Users/nabil/dev/keth/python/cairo-addons/src:\
    /Users/nabil/dev/keth/python/cairo-core/src:\
    /Users/nabil/dev/keth/python/cairo-ec/src:\
    /Users/nabil/dev/keth/cairo:\
    /Users/nabil/dev/keth \
    PYO3_PYTHON=/Users/nabil/dev/keth/.venv/bin/python \
    PYTHONHOME=/Users/nabil/.local/share/uv/python/cpython-3.10.16-macos-aarch64-none \
    cargo run -p keth-run

--- crates/keth-run/src/main.rs
+++ crates/keth-run/src/main.rs
@@ -1,22 +1,43 @@
 use pyo3::prelude::*;
-use std::env;
 
+const OUTPUT_DIR: &str = "output";
+const DATA_DIR: &str = "data/1/eels";
+const COMPILED_PROGRAM: &str = "build/main_compiled.json";
+const BLOCK_NUMBER: u64 = 21922904; // 500M steps
 fn main() -> PyResult<()> {
-    pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
         // Get the current directory and add it to Python's path
-        let mut current_dir = env::current_dir()?;
-        current_dir.push("python/cairo-addons/src/");
+        let current_dir = std::env::current_dir()?;
+        // Add the cairo directory to Python's path
         let sys = PyModule::import(py, "sys")?;
         let path = sys.getattr("path")?;
         path.call_method1("append", (current_dir.to_str().unwrap(),))?;
 
+        /*
         let module = PyModule::import(py, "cairo_addons.compiler")?;
         let test_value = module.getattr("TEST")?;
         println!("TEST value: {}", test_value);
+        */
 
-        // let main_func = module.getattr("main")?;
-        // main_func.call0()?;
+        let zkpi_path = format!("{DATA_DIR}/{BLOCK_NUMBER}.json");
+        let prove_block_module = PyModule::import(py, "cairo.scripts.prove_block")?;
+        let load_zkpi_fixture = prove_block_module.getattr("load_zkpi_fixture")?;
+        let program_inputs = load_zkpi_fixture.call1((zkpi_path,))?;
+
+        let run_proof_mode = prove_block_module.getattr("run_proof_mode")?;
+        println!("calling run_proof_mode");
+
+        run_proof_mode.call1((
+            "main",
+            program_inputs,
+            COMPILED_PROGRAM,
+            OUTPUT_DIR,
+            false,
+            None::<&str>,
+            false,
+        ))?;
+        println!("run_proof_mode done");
+
         Ok(())
     })
 }
