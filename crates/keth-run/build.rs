8bbcfbf9c821b6f2d7b37a63f8d3a99060e297bb
Nabil Elqatib <nabil@kakarot.org>
2025-04-07 16:33:14 +0200

    Use dotenv to load and check the necessary env vars from .env

--- crates/keth-run/build.rs
+++ crates/keth-run/build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    std::env::var("PYTHONPATH").expect("PYTHONPATH environment variable not set");
-    std::env::var("PYTHONHOME").expect("PYTHONHOME environment variable not set");
-    std::env::var("PYO3_PYTHON").expect("PYO3_PYTHON environment variable not set");
+    dotenv::dotenv().ok();
+
+    dotenv::var("PYTHONPATH").expect("PYTHONPATH environment variable not set");
+    dotenv::var("PYTHONHOME").expect("PYTHONHOME environment variable not set");
+    dotenv::var("PYO3_PYTHON").expect("PYO3_PYTHON environment variable not set");
+
+    println!("cargo:rerun-if-env-changed=PYTHONPATH");
+    println!("cargo:rerun-if-env-changed=PYTHONHOME");
+    println!("cargo:rerun-if-env-changed=PYO3_PYTHON");
 }
