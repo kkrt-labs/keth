080e642a6fd2fd624a86381e6f98930526c98eb0
Nabil Elqatib <nabil@kakarot.org>
2025-04-07 13:16:01 +0200

    Reset compiler.py

--- python/cairo-addons/src/cairo_addons/compiler.py
+++ python/cairo-addons/src/cairo_addons/compiler.py
@@ -12,8 +12,6 @@ from starkware.cairo.lang.compiler.program import CairoHint, Program
 
 from cairo_addons.hints import implementations
 
-TEST = "test"
-
 
 def implement_hints(program: Program):
     return {
