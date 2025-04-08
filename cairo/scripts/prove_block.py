8c9a2b462ff8157d38deb7bba9cf12cdaedb792f
Nabil Elqatib <nabil@kakarot.org>
2025-04-08 22:13:05 +0200

    Only dereference proof_path when it's not None

--- cairo/scripts/prove_block.py
+++ cairo/scripts/prove_block.py
@@ -340,7 +340,8 @@ def prove_block(
     program_inputs = load_zkpi_fixture(zkpi_path)
 
     # Generate proof
-    proof_path = Path(proof_path)
+    if proof_path is not None:
+        proof_path = Path(proof_path)
     logger.info(f"Running Keth for block {block_number}")
     run_proof_mode(
         entrypoint="main",
