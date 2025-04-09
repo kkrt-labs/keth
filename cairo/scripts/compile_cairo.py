7255eb8586b445507be9f0a29862d2b858ae2b5f
Clément Walter <clement0walter@gmail.com>
2025-04-09 16:29:24 +0200

    Refacot compile script to share parser and add options to compile keth

--- cairo/scripts/compile_cairo.py
+++ cairo/scripts/compile_cairo.py
@@ -12,10 +12,24 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def compile_cairo(file_name, should_implement_hints=True):
-    input_path = Path(file_name)
-    output_path = input_path.with_suffix(".json")
-    program = cairo_compile(input_path)
+def compile_cairo(
+    path,
+    debug_info=False,
+    proof_mode=True,
+    should_implement_hints=True,
+    output_path=None,
+):
+    logger.info(
+        f"Compiling {path} with\n{debug_info=}\n{proof_mode=}\n{should_implement_hints=}\n{output_path=}"
+    )
+    input_path = Path(path)
+    if output_path is None:
+        output_path = input_path.with_suffix(".json")
+    program = cairo_compile(
+        path=input_path,
+        debug_info=debug_info,
+        proof_mode=proof_mode,
+    )
     if should_implement_hints:
         program.hints = implement_hints(program)
 
@@ -23,42 +37,60 @@ def compile_cairo(file_name, should_implement_hints=True):
         json.dump(program.Schema().dump(program), f, indent=4, sort_keys=True)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Compile Cairo program")
-    parser.add_argument("file_name", help="The Cairo file to compile")
-    parser.add_argument(
-        "--no-implement-hints",
-        action="store_false",
-        dest="implement_hints",
-        default=True,
-        help="Do not implement hints in the compiled program",
-    )
+parser = argparse.ArgumentParser(description="Compile Cairo program")
+parser.add_argument(
+    "--debug-info",
+    action="store_true",
+    default=False,
+    help="Include debug information in the compiled program",
+)
 
+parser.add_argument(
+    "--implement-hints",
+    action="store_true",
+    dest="implement_hints",
+    default=True,
+    help="Implement hints in the compiled program",
+)
+
+
+def main():
+    parser.add_argument(
+        "path",
+        default="cairo/ethereum/cancun/main.cairo",
+        help="The Cairo file to compile",
+    )
+    parser.add_argument(
+        "--proof-mode",
+        action="store_true",
+        default=False,
+        help="Compile in proof mode",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        help="The path to save the compiled program",
+        required=False,
+    )
     args = parser.parse_args()
-    compile_cairo(args.file_name, args.implement_hints)
+    compile_cairo(
+        args.path,
+        args.debug_info,
+        args.proof_mode,
+        args.implement_hints,
+        args.output_path,
+    )
 
 
 def compile_keth():
-    parser = argparse.ArgumentParser(description="Compile Cairo program")
-    parser.add_argument(
-        "--debug-info",
-        action="store_true",
-        default=False,
-        help="Include debug information in the compiled program",
-    )
     args = parser.parse_args()
-    keth_main_path = Path("cairo/ethereum/cancun/main.cairo")
-    proof_mode = True
-    output_path = Path("build/main_compiled.json")
-    logger.info(
-        f"Compiling Keth with debug_info={args.debug_info} and proof_mode={proof_mode}"
+    compile_cairo(
+        Path("cairo/ethereum/cancun/main.cairo"),
+        debug_info=args.debug_info,
+        proof_mode=True,
+        should_implement_hints=args.implement_hints,
+        output_path=Path("build/main_compiled.json"),
     )
-    program = cairo_compile(
-        keth_main_path, debug_info=args.debug_info, proof_mode=proof_mode
-    )
-    with open(output_path, "w") as f:
-        logger.info(f"Writing compiled program to {output_path}")
-        json.dump(program.Schema().dump(program), f, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
